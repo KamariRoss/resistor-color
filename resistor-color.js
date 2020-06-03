eda4630a664e458e62761098955e131d461b7780
KamariRoss <kamarimobile@gmail.com>
2020-06-02 21:29:23 -0400

    Completed Excercism

---
+++ resistor-color.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (index) => {
+  // No help received
+  return COLORS.indexOf(index);
+};
+
+export const COLORS = [
+  // Got help from online on moving the array to the COLORS function
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
